dbdc7bee1e6a31a414bb19ee2bcb7333e9b091e4
The One Reborn <mortonmarrion@gmail.com>
2024-10-05 15:16:37 +0300

    Feat: Get all level two managers

---
+++ app/database/queue/get_all_level_two_managers.py
@@ -0,0 +1,31 @@
+from app.database.queue.connect_to_database import connect_to_database
+from app.database.queue.close_connection import close_connection
+
+
+async def get_all_level_two_managers() -> list | None:
+    """
+    Get a list of all chat IDs of level two managers.
+
+    Returns a list of all chat IDs of level two managers if successful, None otherwise.
+    """
+    conn = await connect_to_database()
+    try:
+        managers = await conn.fetch(
+            """
+            SELECT
+                chat_id
+            FROM
+                employees
+            WHERE
+                manager_level = 2
+            """
+        )
+
+        chat_ids = [manager['chat_id'] for manager in managers]
+        print(f'Successfully fetched all level two managers: {chat_ids}')
+        return chat_ids
+    except Exception as e:
+        print(f"Error getting all level two managers: {e}")
+        return None
+    finally:
+        await close_connection(conn)
